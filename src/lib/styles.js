419e420cafb802eba7b6c3552e04ccc64100e64e
Phillip Casimir <philcprojx@gmail.com>
2026-05-05 18:38:30 -0400

    Implemented Project Tasks feature with CRUD functionality and make some improvements to Dashboard UI and layout.

--- src/lib/styles.js
+++ src/lib/styles.js
@@ -1,6 +1,6 @@
 export const styles = {
     layout: {
-        page: "flex-1 p-8 bg-slate-100 min-h-screen",
+        page: "flex-1 p-4 md:p-8 bg-slate-100 min-h-screen",
         section: "bg-white rounded-2xl shadow-sm border border-slate-200 p-6",
     },
 
@@ -23,7 +23,7 @@ export const styles = {
             "flex gap-2 bg-emerald-600 text-white px-4 py-2 rounded-lg hover:bg-emerald-500 transition",
         secondary:
             "flex gap-2 rounded-lg border border-slate-300 px-3 py-2 text-slate-700 hover:bg-slate-100 transition",
-        redBtn: 
+        redBtn:
             "bg-red-600 text-white px-4 py-2 rounded-lg hover:bg-red-500 transition",
 
     },
@@ -38,7 +38,7 @@ export const styles = {
     },
 
     grid: {
-        stats: "grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mb-8",
+        stats: "grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6 mb-8",
         main: "grid grid-cols-1 lg:grid-cols-3 gap-6",
         projects: "grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6",
         detailsTwoCol: "mb-8 grid grid-cols-1 gap-6 md:grid-cols-2"
